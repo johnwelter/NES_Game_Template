604de7d6d139f64d82a84942721a97464f36cff8
rockyhardwood <rockyhardwood6900@gmail.com>
2025-01-09 21:54:00 +0100

    title screen initial methods

--- Picross/Routines/Game_States/UpdateTitle.asm
+++ Picross/Routines/Game_States/UpdateTitle.asm
@@ -1,15 +1,38 @@
 UpdateTitle:
 
   LDA NMI_locks
-  BNE .leave
+  BEQ .unlocked
+  
+  RTS
+  
+.unlocked:
 
-  INC PPU_ScrollX
-  INC PPU_ScrollX
-  BNE .updateBankSelection
-  ;;flip bit in our PPU scroll loop thingy
-  LDA PPU_ScrollNT
-  EOR #%00000001
-  STA PPU_ScrollNT
+  JSR DoUpdateTitle
+  
+  LDA gamepadPressed
+  AND #GAMEPAD_START
+  BNE .loadGame
+  RTS
+  
+.loadGame:
+  LDA #GAME_IDX
+  JSR ChangeGameMode
+  
+  RTS
+  
+DoUpdateTitle:
+
+  LDA mode_state
+  JSR Dynamic_Jump
+    
+UpdateTitleJumpTable:
+
+  .word UpdateTitleInit
+  .word UpdateBankSelection
+  .word UpdateScroll
+  .word UpdatePuzzleSelection
+  .word UpdateScrollBack
+
   
 ;;title should have two steps- the bank selection and the puzzle selection
 ;;top half of screen can be used as the title screen, and the bottom half will have the selectable options
@@ -17,17 +40,71 @@ UpdateTitle:
 ;;starting options list: BANK 0, BANK 1, BANK 2, BANK3
 ;;select one, then scroll over to the right, with numbers for puzzles 
 
-.updateBankSelection:
-.updateScroll:
-.updatePuzzleSelection:
+UpdateTitleInit:
+.changeModeState:
 
+  INC mode_state
+.leave:
+  RTS
+  
+UpdateBankSelection:
 
   LDA gamepadPressed
   BEQ .leave
   
-    LDA #GAME_IDX
-	JSR ChangeGameMode
- 
+.changeModeState:
+
+  INC mode_state
 .leave:
- 
+  RTS
+  
+UpdateScroll:
+  INC PPU_ScrollX
+  INC PPU_ScrollX
+  INC PPU_ScrollX
+  INC PPU_ScrollX
+  BNE .leave
+  LDA PPU_ScrollNT
+  EOR #%00000001
+  STA PPU_ScrollNT
+  
+.changeModeState:
+
+  INC mode_state
+.leave:
+  RTS
+  
+UpdatePuzzleSelection:
+
+  LDA gamepadPressed
+  BEQ .leave
+  
+.changeModeState:
+  
+  LDA #$FC
+  STA PPU_ScrollX
+  LDA PPU_ScrollNT
+  AND #$FE
+  STA PPU_ScrollNT
+  
+  INC mode_state
+.leave:
+  RTS
+  
+UpdateScrollBack:
+
+
+  DEC PPU_ScrollX
+  DEC PPU_ScrollX
+  DEC PPU_ScrollX
+  DEC PPU_ScrollX
+  BNE .leave
+  
+.changeModeState:
+
+  DEC mode_state
+  DEC mode_state
+  DEC mode_state
+  
+.leave:
   RTS
